3e9a40d42d63d376b0c8d6cccf2fbeaef3187d51
Sai Srikar Tummala <>
2021-04-21 15:02:24 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+'''
+02/25/2021
+
+List
+A data structure that is mutable(changable) ordered sequence of elements/ Defind by values between square brackets, [ ].
+
+Ex.
+students = ["student1", "student2", "student3", ...]
+
+box = ["Anything", 123, True, 428, - 100]
+print(box)
+
+>> ['Anything', 123, True, 428, - 100]
+
+name = ["John", "Chloe", "Ian", "Ashley"]
+print(name)
+>> ["John", "Chloe", "Ian", 'Ashley']
+'''
